371589207cfaa06c03903fc77b73ef30b7649f06
LBF38 <contact.lbf38@gmail.com>
2024-06-03 19:48:26 +0200

    feat(ui): :sparkles: implement left sidebar
    add sidebar w/ sections and links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,49 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
 	import * as Avatar from '$lib/components/ui/avatar';
+	import * as Collapsible from '$lib/components/ui/collapsible';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
-	import { Menu } from 'lucide-svelte';
-	import * as Sheet from '$lib/components/ui/sheet';
 	import { Input } from '$lib/components/ui/input';
-	import Button from '$components/ui/button/button.svelte';
+	import * as Sheet from '$lib/components/ui/sheet';
+	import { Home, Menu, PenBox, Table } from 'lucide-svelte';
+
+	const sections: {
+		name: string;
+		lucide_icon?: any; // use svelte components for rendering it in UI.
+		//TODO: ^ make it more typed.
+		subsections: { name: string; href: string }[];
+	}[] = [
+		{
+			name: 'Home',
+			lucide_icon: Home,
+			subsections: [
+				{ name: 'Dashboard 1', href: '/' },
+				{ name: 'Dashboard 2', href: '/' },
+				{ name: 'Dashboard 3', href: '/' },
+				{ name: 'Dashboard 4', href: '/' }
+			]
+		},
+		{
+			name: 'Forms',
+			lucide_icon: PenBox,
+			subsections: [
+				{ name: 'Form 1', href: '/' },
+				{ name: 'Form 2', href: '/' },
+				{ name: 'Form 3', href: '/' },
+				{ name: 'Form 4', href: '/' }
+			]
+		},
+		{
+			name: 'Tables',
+			lucide_icon: Table,
+			subsections: [
+				{ name: 'Table 1', href: '/' },
+				{ name: 'Table 2', href: '/' },
+				{ name: 'Table 3', href: '/' },
+				{ name: 'Table 4', href: '/' }
+			]
+		}
+	];
 </script>
 
 <nav class="flex justify-between items-center">
@@ -14,8 +53,28 @@
 		</Sheet.Trigger>
 		<Sheet.Content side="left">
 			<Sheet.Header>
-				<Sheet.Title>WIP on left sidebar</Sheet.Title>
-				<Sheet.Description>TODO: add the links and other parts.</Sheet.Description>
+				<Sheet.Title>Gentellela Alela!</Sheet.Title>
+				<Sheet.Description>
+					{#each sections as { name, lucide_icon, subsections }}
+						<Collapsible.Root class="py-4 w-full">
+							<Collapsible.Trigger class="flex gap-4 items-center w-full">
+								{#if lucide_icon}
+									<svelte:component this={lucide_icon} />
+								{/if}
+								<span class="font-bold">
+									{name}
+								</span>
+							</Collapsible.Trigger>
+							<Collapsible.Content class="my-4">
+								<ul>
+									{#each subsections as { name, href }}
+										<li><Button {href} variant="link">{name}</Button></li>
+									{/each}
+								</ul>
+							</Collapsible.Content>
+						</Collapsible.Root>
+					{/each}
+				</Sheet.Description>
 			</Sheet.Header>
 		</Sheet.Content>
 	</Sheet.Root>

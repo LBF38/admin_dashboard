513dfa07ee09febe86561889074e82b2812fa07b
LBF38 <contact.lbf38@gmail.com>
2024-06-03 08:16:10 +0200

    feat(ui): :sparkles: add first drafts on pages & layout
    add simple signup page w/o form logic
    add avatar and dropdown menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+	import { Toaster } from '$components/ui/sonner';
 	import '../app.pcss';
 </script>
 
-<slot />
+<Toaster richColors />
+<div class="flex min-h-screen flex-col">
+	<main class="container my-4 flex-grow px-4">
+		<slot />
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as Avatar from '$lib/components/ui/avatar';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+</script>
+
+<nav class="">
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger class="flex items-center gap-4">
+			<Avatar.Root>
+				<Avatar.Image src="https://github.com/lbf38.png" alt="@lbf38" />
+				<Avatar.Fallback>LBF38</Avatar.Fallback>
+			</Avatar.Root>
+			<span>LBF38</span>
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content>
+			<DropdownMenu.Group>
+				<DropdownMenu.Label>My Account</DropdownMenu.Label>
+				<DropdownMenu.Separator />
+				<DropdownMenu.Item href="/settings">Settings</DropdownMenu.Item>
+				<DropdownMenu.Item href="/signup">Signup</DropdownMenu.Item>
+				<DropdownMenu.Item href="/login">Login</DropdownMenu.Item>
+			</DropdownMenu.Group>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+</nav>
